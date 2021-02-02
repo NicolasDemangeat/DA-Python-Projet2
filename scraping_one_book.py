323b2be0a88d5a88ecd4f3f59821aac589a3dddc
NicolasPython <demangeatn@gmail.com>
2021-02-02 09:38:49 +0100

    creation de la fonction principal

--- scraping_one_book.py
+++ scraping_one_book.py
@@ -25,24 +25,18 @@ def set_the_url():
 			print("L'URL n'est pas valide, elle doit être de la forme http://books.toscrape.com/catalogue/{nom_du_livre}/index.html")
 	return the_url
 
-try:
+def scrap_one_book():
 	the_url = set_the_url()
 	# set the response resquests
 	response = requests.get(the_url)
 	# if OK, scrap the page
 	if response.ok:
 		product_page_url = the_url
-
-		soup = BeautifulSoup(response.content, 'html.parser') 
-		
-		title = soup.h1.string	# scrap of title
-		
-		image_url = urllib.parse.urljoin("http://books.toscrape.com/", soup.img['src']) # scrap of URL image
-		
-		category = soup.find('ul')('li')[2].text.strip() # scrap gategory
-		
-		all_p = soup.findAll('p') # find all paragraph
-		
+		soup = BeautifulSoup(response.content, 'html.parser')			
+		title = soup.h1.string	# scrap of title			
+		image_url = urllib.parse.urljoin("http://books.toscrape.com/", soup.img['src']) # scrap of URL image			
+		category = soup.find('ul')('li')[2].text.strip() # scrap gategory			
+		all_p = soup.findAll('p') # find all paragraph			
 		product_description = all_p[3].text #in all paragraph, p[3] is the description
 		# find each paragraph in all paragraph
 		for p in all_p:
@@ -50,18 +44,14 @@ try:
 			if rewiev_p[0] == 'star-rating': # condition to find the paragraph with class == star rating
 				review_rating = rewiev_p[1]	# select the number rating	
 				break 
-		
+			
 		tds = soup.find_all('td') #find all td
 		universal_product_code = tds[0].text
-
-		price_excluding_tax = tds[2].text 
-
+		price_excluding_tax = tds[2].text
 		price_including_tax = tds[3].text
-
 		number_available = tds[5].text[10:12] #select only the number TODO : modif avec regex ?
 
-
-	book_info = {'product_page_url': product_page_url,
+		return {'product_page_url': product_page_url,
 				'universal_product_code(upc)': universal_product_code,
 				'title': title,
 				'price_including_tax': price_including_tax,
@@ -72,6 +62,8 @@ try:
 				'review_rating': review_rating,
 				'image_url': image_url}
 
+try:	
+	book_info = scrap_one_book()
 	save_info_in_csv(book_info)
 except NameError:
 	print("ERREUR : Le livre est introuvable, veuillez relancer le script en vérifiant l'URL.")
